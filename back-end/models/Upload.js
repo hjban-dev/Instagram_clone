6d2522d345ddb5e2ef9df74c00ea5307c2fc7612
hjban <baneju212@gmail.com>
2021-01-12 17:21:29 +0900

    Upload 모델 생성

---
+++ back-end/models/Upload.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const uploadSchema = Schema(
+	{
+		writer: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+		title: {
+			type: String,
+			maxlength: 250,
+		},
+		description: {
+			type: String,
+		},
+		privacy: {
+			type: Number,
+		},
+		filePath: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Upload = mongoose.model("Upload", uploadSchema);
+module.exports = { Upload };
